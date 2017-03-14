34032e107616c4d37e725d6beb5ca07aa9785ae3
RNogales <xrnogales@gmail.com>
2017-03-14 17:54:44 +0100

    Update cleaning

--- Cleaning.py
+++ Cleaning.py
@@ -3,6 +3,14 @@ import nltk
 from nltk.corpus import stopwords
 import nltk.collocations
 import collections
+import re, string
+import pandas
+
+def remove_punctuation ( text ):
+    return re.sub('[%s]' % re.escape(string.punctuation), ' ', text)
+
+
+
 f = open('./Datasets/training_info_sid_min.csv', 'r')
 reader = csv.reader(f, delimiter=',', quotechar='"')
 body_list = []
@@ -11,13 +19,15 @@ for row in reader:
 
 body_clean_list = []
 for email in body_list:
+    email = remove_punctuation(email)
     body_clean_list.append([str.lower(word) for word in str.split(email) if (str.lower(word) not in stopwords.words('english'))])
-    
+
+"""
 for email in body_clean_list:
-    print(email)
+    print(*email)
     print("#######################################################")
+"""    
 
+pd = pandas.DataFrame(body_clean_list)
+pd.to_csv("out.csv")
 
-
-##bgm = nltk.collocations.BigramAssocMeasures()
-##finder = nltk.collocations.BigramCollocationFinder.from_words(documents.split())
