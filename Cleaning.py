44bf648fc6cd1075e67bb0c85d6dc55e49e31f17
RNogales94 <rnogales@correo.ugr.es>
2017-04-06 06:20:25 +0200

    Add Word to Vector

--- Cleaning.py
+++ Cleaning.py
@@ -15,9 +15,9 @@ from TextUtils import *
 training_info_sid_min.csv C'est un fichier qui est un extrait du fichier training_info_sid.csv
 On l'a fait pour minimiser les temps du execution
 """
-f = open('./Datasets/new_training_info.csv', 'r')
+f = open('./Datasets/new_training_info_min.csv', 'r')
 reader = csv.reader(f, delimiter=',', quotechar='"')
-steamer = Steamer()
+steamer = DocTransformer()
 
 
 """
@@ -33,12 +33,14 @@ Initialisation des listes des champs pour email
 body_list = []
 destinataires_list = []
 expediteurs_list = []
+
+C = 0 #PARAMETER:  TAKE C = 0 IF THERE ISN'T INDEX COLUMN IN THE CSV // ELSE: C = 1 
 for row in reader:
-    id_list.append(row[1])
-    date_list.append(row[2])
-    body_list.append(row[3])
-    destinataires_list.append(row[4])
-    expediteurs_list.append(row[5])
+    id_list.append(row[0+C])
+    date_list.append(row[1+C])
+    body_list.append(row[2+C])
+    destinataires_list.append(row[3+C])
+    expediteurs_list.append(row[4+C])
 
 #(DATE)
 """
@@ -68,7 +70,7 @@ Initialisation des emails
 """
 email_list = []    
 for i in range(0,len(id_list)):
-    email_list.append(Email(ID_mail=id_list[i], text=body_list[i], date=date_list[i], destinataires=destinataires_list[i],expediteurs=expediteurs_list[i].split()))
+    email_list.append(Email(ID_mail=id_list[i], text=body_list[i], date=datetime.date(year_date_list[i],month_date_list[i],day_date_list[i]), destinataires=destinataires_list[i],expediteurs=expediteurs_list[i].split()))
 
 """
 Nettoyage des emails (le text nettoyé s'est mis sur email.tokenise comme un vecteur des mots)
@@ -76,7 +78,45 @@ Nettoyage des emails (le text nettoyé s'est mis sur email.tokenise comme un vec
 for email in email_list:
     email.text = steamer.clean(*email.text)
     email.tokenise = steamer.removeStopWords(steamer.tokenise(email.text))
-    email.print()
+    #email.print()
+
+    
+"""
+Initialisation pour TFIDF:
+"""
+
+tfidf_maker_body = DocTransformer()
+tfidf_maker_expediteurs = DocTransformer()
+documents = [email.text  for email in email_list]
+expediteurs = [str(email.expediteurs) for email in email_list]
+dates = [[email.date[0].month, email.date[0].year, email.date[0].isoweekday(), email.date[0].day] for email in email_list]
+#print(dates)
+
+tfidf_maker_body.getVectorKeywordIndex(documents)
+tfidf_maker_body.matrixVector(documents)
+tfidf_maker_expediteurs.getVectorKeywordIndex(expediteurs)
+tfidf_maker_expediteurs.matrixVector(expediteurs)
+
+matrix_tfidf_body = tfidf_maker_body.documentVectors
+matrix_tfidf_expediteurs = tfidf_maker_expediteurs.documentVectors
+
+#for row in matrix_tfidf_expediteurs:
+#    print(row)
+#print(sum(matrix_tfidf_body[1]))
+#print(sum(matrix_tfidf_expediteurs[1]))
+#print("########")
+matrix_data = []
+for i in range(len(matrix_tfidf_body)):
+    matrix_data.append(np.concatenate((matrix_tfidf_body[i],matrix_tfidf_expediteurs[i],dates[i])))
+
+print(matrix_data[1])
+
+
+
+
+
+
+
 
 """
 pd = pandas.DataFrame(id_list, date_list, destinataires_list, body_clean_list)
--- TextUtils.py
+++ TextUtils.py
@@ -1,10 +1,19 @@
+import numpy as np
 from stemming.porter2 import stem
 import re
 from sklearn.feature_extraction.text import TfidfTransformer
 
-class Steamer:
-    #vectorKeywordIndex = []
-    #documentVectors = []
+def cosineVector(vector1, vector2):
+        """ related documents j and q are in the concept space by comparing the vectors :cosine  = ( V1 * V2 ) / ||V1|| x ||V2|| """
+        if (np.linalg.norm(vector1) != 0) and (np.linalg.norm(vector2) != 0):
+            return float(np.dot(vector1,vector2) / (np.linalg.norm(vector1) * np.linalg.norm(vector2)))
+        else:
+            return 0
+
+
+class DocTransformer:
+    vectorKeywordIndex = []
+    documentVectors = []
     def __init__(self):
         self.stopwords= open('./doc/SmartStoplist.txt','r').read().split()
         
@@ -31,6 +40,46 @@ class Steamer:
         return [stem(word) for word in words]
         #2/Stoplist
 
-    def removeStopWords(self,list):
+    def removeStopWords(self, list):
         """ Remove common words which have no search value """
         return [word for word in list if word not in self.stopwords ]
+    
+    def getVectorKeywordIndex(self, documentList):
+        """ create the keyword associated to the position of the elements within the document vectors """
+        #Mapped documents into a single word string
+        vocabularyString = " ".join(documentList)
+
+        vocabularyList = self.tokenise(vocabularyString)
+        #Remove common words which have no search value
+        vocabularyList = self.removeStopWords(vocabularyList)
+        #Supprime les mots multiples
+        uniqueVocabularyList = list(set(vocabularyList))
+
+        vectorIndex={}
+        offset=0
+        #Associate a position with the keywords which maps to the dimension on the vector used to represent this word
+        for word in uniqueVocabularyList:
+                vectorIndex[word]=offset
+                offset+=1
+        self.vectorKeywordIndex =vectorIndex
+        return vectorIndex  #(keyword:position)
+    #4/tf-idf et creer les vecteurs
+    #Va renvoyer un vecteur avec le score de chaque terme
+    def makeVector(self, wordString):
+        #Initialise vector with 0's
+        vector = [0] * len(self.vectorKeywordIndex)
+        wordList = self.tokenise(wordString)
+        wordList = self.removeStopWords(wordList)
+        N = len(wordString)+1 #+1 POUR EVITER DIVISION ENTRE 0
+        for word in wordList:
+                vector[self.vectorKeywordIndex[word]] += 1/N; #Use simple Term Count Model
+        return vector
+
+
+    def matrixVector(self, documents):
+        """ Create the vector space for the passed document strings """
+        matrix = [self.makeVector(document) for document in documents]
+        transformer = TfidfTransformer(smooth_idf=True)
+        tfidf = transformer.fit_transform(matrix)       
+        self.documentVectors = tfidf.toarray().tolist()
+        print("Matrice des vecteurs de chaque paragraphe du document crée")
